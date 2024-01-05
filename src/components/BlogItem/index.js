d37c8abfcc6515a583797cc0da38f2427faba1db
ksesikumar <ksesikumar7@gmail.com>
2024-01-05 17:24:42 +0000

    Blog List App

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {eachBlog} = props
+  const {title, description, publishedDate} = eachBlog
+  return (
+    <li className="items-container">
+      <div className="title-and-publishedDate">
+        <h1 className="blog-title">{title}</h1>
+        <p className="blog-published-date">{publishedDate}</p>
+      </div>
+      <p className="blog-description">{description}</p>
+      <hr className="line" />
+    </li>
+  )
+}
+
+export default BlogItem
